df62147515f2ed2dd77fc577d02b7e2ad5277c54
CaptainAG <85942725+CaptainAG@users.noreply.github.com>
2023-08-22 10:25:02 +0200

    correction ficher "function.php"

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package _s
  */
 
-if ( ! defined( '_WEBPACK_THEME_VERSION' ) ) {
+if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_WEBPACK_THEME_VERSION', '1.0.0' );
+	define( '_S_VERSION', '1.0.0' );
 }
 
 /**
@@ -19,14 +19,14 @@ if ( ! defined( '_WEBPACK_THEME_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function _Webpack_theme_setup() {
+function _s_setup() {
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on _s, use a find and replace
 		* to change '_s' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( '_Webpack_theme', get_template_directory() . '/languages' );
+	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
@@ -49,7 +49,7 @@ function _Webpack_theme_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', '_Webpack_theme' ),
+			'menu-1' => esc_html__( 'Primary', '_s' ),
 		)
 	);
 
@@ -74,7 +74,7 @@ function _Webpack_theme_setup() {
 	add_theme_support(
 		'custom-background',
 		apply_filters(
-			'_Webpack_theme_custom_background_args',
+			'_s_custom_background_args',
 			array(
 				'default-color' => 'ffffff',
 				'default-image' => '',
@@ -115,19 +115,19 @@ function _s_content_width() {
 	$GLOBALS['content_width'] = apply_filters( '_s_content_width', 640 );
 }
 add_action( 'after_setup_theme', '_s_content_width', 0 );
-*/
+
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function _Webpack_theme_widgets_init() {
+function _s_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', '_Webpack_theme' ),
+			'name'          => esc_html__( 'Sidebar', '_s' ),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', '_Webpack_theme' ),
+			'description'   => esc_html__( 'Add widgets here.', '_s' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -135,21 +135,21 @@ function _Webpack_theme_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', '_Webpack_theme_widgets_init' );
+add_action( 'widgets_init', '_s_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function _Webpack_theme_scripts() {
-	wp_enqueue_style( '_Webpack_theme-style', get_stylesheet_uri(), array(), _Webpack_theme_VERSION );
+function _s_scripts() {
+	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_enqueue_script( '_Webpack_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _Webpack_theme_VERSION, true );
+	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', '_Webpack_theme_scripts' );
+add_action( 'wp_enqueue_scripts', '_s_scripts' );
 
 /**
  * Implement the Custom Header feature.
